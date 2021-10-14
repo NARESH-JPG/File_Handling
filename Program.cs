2c4833757193b557131229669bad2193ff56e055
NARESH <64158150+NARESH-JPG@users.noreply.github.com>
2021-10-14 14:04:49 +0530

    Day_3

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+ 
+namespace File_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File handling......!!!");
+            FileStream fs = new FileStream("D:\\.NET\\C#\\Day_3\\File_Handling\\file.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            Console.WriteLine("we can read and display from file");
+            string str = sr.ReadToEnd();
+            while (str != null)
+            {
+                Console.WriteLine(str);
+                str = sr.ReadLine();
+            }
+
+            fs.Flush();
+            fs.Close();
+        
+
+
+            FileStream fwrite = new FileStream("D:\\.NET\\C#\\Day_3\\File_Handling\\file.txt", FileMode.Append);
+            StreamWriter Swrite= new StreamWriter(fwrite);
+            Console.WriteLine("Enter your content to file...");
+         
+            string content = Console.ReadLine();
+
+            Swrite.Write(content);
+
+
+            Swrite.Flush();
+            fwrite.Flush();
+            fwrite.Close();
+
+
+        }
+    }
+}
